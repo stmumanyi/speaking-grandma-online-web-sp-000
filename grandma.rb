f32e6b1d8ca2d5933836ff851c6d80d37b84e10f
Simba Mumanyi <simbamumanyi@gmail.com>
2020-06-25 14:34:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.upcase  
+  elsif string ==  
     return "NO, NOT SINCE 1938!"
   else string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
