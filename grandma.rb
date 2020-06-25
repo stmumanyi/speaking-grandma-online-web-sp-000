1545493db4ef4c2c9e04775ed88191a7792acb77
Simba Mumanyi <simbamumanyi@gmail.com>
2020-06-25 14:34:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string ==  
     return "NO, NOT SINCE 1938!"
-  else string == "I LOVE YOU GRANDMA!"
+  else string == 
     return "I LOVE YOU TOO PUMPKIN!"
 end
 end
